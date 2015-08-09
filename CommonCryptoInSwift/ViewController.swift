0f2f49206b8efa07b90a1d6a39444d7dcb0aa8f2
Ignacio Nieto <contact@digitalleaves.com>
2015-08-09 21:19:37 +0200

    Initial Commit

---
+++ CommonCryptoInSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CommonCryptoInSwift
+//
+//  Created by Ignacio Nieto Carvajal on 9/8/15.
+//  Copyright © 2015 Ignacio Nieto Carvajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
