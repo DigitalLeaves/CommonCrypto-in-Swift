447b90b16daa641bb82197dc316a83106103f60b
Ignacio Nieto <contact@digitalleaves.com>
2015-08-10 16:22:01 +0200

    Initial commit

--- CommonCryptoInSwift/SymmetricCryptor.swift
+++ CommonCryptoInSwift/SymmetricCryptor.swift
@@ -1,5 +1,5 @@
 //
-//  CommonCryptoManager.swift
+//  SymmetricCryptor
 //  CommonCryptoInSwift
 //
 //  Created by Ignacio Nieto Carvajal on 9/8/15.
@@ -8,63 +8,188 @@
 
 import UIKit
 
+private let kSymmetricCryptorRandomStringGeneratorCharset: [Character] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".characters.map({$0})
+
+enum SymmetricCryptorAlgorithm {
+    case DES        // DES standard, 64 bits key
+    case DES40      // DES, 40 bits key
+    case TRIPLEDES  // 3DES, 192 bits key
+    case RC4_40     // RC4, 40 bits key
+    case RC4_128    // RC4, 128 bits key
+    case RC2_40     // RC2, 40 bits key
+    case RC2_128    // RC2, 128 bits key
+    case AES_128    // AES, 128 bits key
+    case AES_256    // AES, 256 bits key
+    
+    // returns the CCAlgorithm associated with this SymmetricCryptorAlgorithm
+    func ccAlgorithm() -> CCAlgorithm {
+        switch (self) {
+        case DES: return CCAlgorithm(kCCAlgorithmDES)
+        case DES40: return CCAlgorithm(kCCAlgorithmDES)
+        case TRIPLEDES: return CCAlgorithm(kCCAlgorithm3DES)
+        case RC4_40: return CCAlgorithm(kCCAlgorithmRC4)
+        case RC4_128: return CCAlgorithm(kCCAlgorithmRC4)
+        case RC2_40: return CCAlgorithm(kCCAlgorithmRC2)
+        case RC2_128: return CCAlgorithm(kCCAlgorithmRC2)
+        case AES_128: return CCAlgorithm(kCCAlgorithmAES)
+        case AES_256: return CCAlgorithm(kCCAlgorithmAES)
+        }
+    }
+    
+    // Returns the needed size for the IV to be used in the algorithm (0 if no IV is needed).
+    func requiredIVSize(options: CCOptions) -> Int {
+        // if kCCOptionECBMode is specified, no IV is needed.
+        if options & CCOptions(kCCOptionECBMode) != 0 { return 0 }
+        // else depends on algorithm
+        switch (self) {
+        case DES: return kCCBlockSizeDES
+        case DES40: return kCCBlockSizeDES
+        case TRIPLEDES: return kCCBlockSize3DES
+        case RC4_40: return 0
+        case RC4_128: return 0
+        case RC2_40: return kCCBlockSizeRC2
+        case RC2_128: return kCCBlockSizeRC2
+        case AES_128: return kCCBlockSizeAES128
+        case AES_256: return kCCBlockSizeAES128 // AES256 still requires 256 bits IV
+        }
+    }
+    
+    func requiredKeySize() -> Int {
+        switch (self) {
+        case DES: return kCCKeySizeDES
+        case DES40: return 5 // 40 bits = 5x8
+        case TRIPLEDES: return kCCKeySize3DES
+        case RC4_40: return 5
+        case RC4_128: return 16 // RC4 128 bits = 16 bytes
+        case RC2_40: return 5
+        case RC2_128: return kCCKeySizeMaxRC2 // 128 bits
+        case AES_128: return kCCKeySizeAES128
+        case AES_256: return kCCKeySizeAES256
+        }
+    }
+    
+    func requiredBlockSize() -> Int {
+        switch (self) {
+        case DES: return kCCBlockSizeDES
+        case DES40: return kCCBlockSizeDES
+        case TRIPLEDES: return kCCBlockSize3DES
+        case RC4_40: return 0
+        case RC4_128: return 0
+        case RC2_40: return kCCBlockSizeRC2
+        case RC2_128: return kCCBlockSizeRC2
+        case AES_128: return kCCBlockSizeAES128
+        case AES_256: return kCCBlockSizeAES128 // AES256 still requires 128 bits IV
+        }
+    }
+}
+
+enum SymmetricCryptorError: ErrorType {
+    case MissingIV
+    case CryptOperationFailed
+    case WrongInputData
+    case UnknownError
+}
+
 class SymmetricCryptor: NSObject {
     // properties
-    var algorithm: CCAlgorithm      // Algorithm
-    var options: CCOptions          // Options (i.e: kCCOptionECBMode + kCCOptionPKCS7Padding)
-    var iv: NSData?                 // Initialization Vector
+    var algorithm: SymmetricCryptorAlgorithm    // Algorithm
+    var options: CCOptions                      // Options (i.e: kCCOptionECBMode + kCCOptionPKCS7Padding)
+    var iv: NSData?                             // Initialization Vector
 
-    init(algorithm: Int, options: Int) {
-        self.algorithm = CCAlgorithm(algorithm)
+    init(algorithm: SymmetricCryptorAlgorithm, options: Int) {
+        self.algorithm = algorithm
         self.options = CCOptions(options)
     }
     
-    convenience init(algorithm: Int, options: Int, iv: String, encoding: UInt = NSUTF8StringEncoding) {
+    convenience init(algorithm: SymmetricCryptorAlgorithm, options: Int, iv: String, encoding: UInt = NSUTF8StringEncoding) {
         self.init(algorithm: algorithm, options: options)
         self.iv = iv.dataUsingEncoding(encoding)
     }
     
-    func crypt(string: String, key: String) {
-        cryptoOperation(string, key: key, operation: CCOperation(kCCEncrypt))
+    func crypt(string string: String, key: String) throws -> NSData {
+        do {
+            if let data = string.dataUsingEncoding(NSUTF8StringEncoding) {
+                return try self.cryptoOperation(data, key: key, operation: CCOperation(kCCEncrypt))
+            } else { throw SymmetricCryptorError.WrongInputData }
+        } catch {
+            throw(error)
+        }
     }
     
-    func decrypt(string: String, key: String) {
-        cryptoOperation(string, key: key, operation: CCOperation(kCCDecrypt))
+    func crypt(data data: NSData, key: String) throws -> NSData {
+        do {
+            return try self.cryptoOperation(data, key: key, operation: CCOperation(kCCEncrypt))
+        } catch {
+            throw(error)
+        }
     }
     
-    internal func cryptoOperation(string: String, key: String, operation: CCOperation) -> NSData {
-        // Data parameters
+    func decrypt(data: NSData, key: String) throws -> NSData  {
+        do {
+            return try self.cryptoOperation(data, key: key, operation: CCOperation(kCCDecrypt))
+        } catch {
+            throw(error)
+        }
+    }
+    
+    internal func cryptoOperation(inputData: NSData, key: String, operation: CCOperation) throws -> NSData {
+        // Validation checks.
+        if iv == nil && (self.options & CCOptions(kCCOptionECBMode) == 0) {
+            throw(SymmetricCryptorError.MissingIV)
+        }
+        
+        // Prepare data parameters
         let keyData: NSData! = key.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
         let keyBytes         = UnsafePointer<Void>(keyData.bytes)
-        let keyLength        = size_t(kCCKeySize3DES)
-        let stringAsData     = string.dataUsingEncoding(NSUTF8StringEncoding)!
-        let dataLength       = Int(stringAsData.length)
-        let dataBytes        = UnsafePointer<Void>(stringAsData.bytes)
-        let bufferData       = NSMutableData(length: Int(dataLength) + kCCBlockSize3DES)!
+        let keyLength        = size_t(algorithm.requiredKeySize())
+        let dataLength       = Int(inputData.length)
+        let dataBytes        = UnsafePointer<Void>(inputData.bytes)
+        let bufferData       = NSMutableData(length: Int(dataLength) + algorithm.requiredBlockSize())!
         let bufferPointer    = UnsafeMutablePointer<Void>(bufferData.mutableBytes)
         let bufferLength     = size_t(bufferData.length)
-        let noIV = UnsafePointer<Void>(iv?.bytes ?? nil)
+        let ivBuffer         = iv == nil ? nil : UnsafePointer<Void>(iv!.bytes)
         var bytesDecrypted = Int(0)
+        // Perform operation
         let cryptStatus = CCCrypt(
-            operation,          // Operation
-            algorithm,          // Algorithm
-            options,            // Options (ECB + PKCS7 Padding)
-            keyBytes,           // key data
-            keyLength,          // key length
-            noIV,               // 0 (null) IV, as we are using ECB
-            dataBytes,          // input data
-            dataLength,         // input length
-            bufferPointer,      // output buffer
-            bufferLength,       // output buffer length
-            &bytesDecrypted)  // output bytes decrypted real length
-        if UInt32(cryptStatus) == UInt32(kCCSuccess) {
+            operation,                  // Operation
+            algorithm.ccAlgorithm(),    // Algorithm
+            options,                    // Options
+            keyBytes,                   // key data
+            keyLength,                  // key length
+            ivBuffer,                   // IV buffer
+            dataBytes,                  // input data
+            dataLength,                 // input length
+            bufferPointer,              // output buffer
+            bufferLength,               // output buffer length
+            &bytesDecrypted)            // output bytes decrypted real length
+        if Int32(cryptStatus) == Int32(kCCSuccess) {
             bufferData.length = bytesDecrypted // Adjust buffer size to real bytes
-            print("Decyphered: \(bufferData as NSData)\n")
-            if let asstring = NSString(data: bufferData as NSData, encoding: NSUTF8StringEncoding) { print("As string: \(asstring)\n") }
             return bufferData as NSData
         } else {
-            print("Error: \(cryptStatus)\n")
-            return NSData()
+            print("Error in crypto operation: \(cryptStatus)")
+            throw(SymmetricCryptorError.CryptOperationFailed)
         }
+    }
+    
+    // MARK: - Random methods
+    
+    class func randomDataOfLength(length: Int) -> NSData? {
+        let mutableData = NSMutableData(length: length)!
+        let bytes = UnsafeMutablePointer<UInt8>(mutableData.mutableBytes)
+        let status = SecRandomCopyBytes(kSecRandomDefault, length, bytes)
+        return status == 0 ? mutableData as NSData : nil
+    }
+    
+    class func randomStringOfLength(length:Int) -> String {
+        var string = ""
+        for _ in (1...length) {
+            string.append(kSymmetricCryptorRandomStringGeneratorCharset[Int(arc4random_uniform(UInt32(kSymmetricCryptorRandomStringGeneratorCharset.count) - 1))])
+        }
+        return string
+    }
+    
+    func setRandomIV() {
+        let length = self.algorithm.requiredIVSize(self.options)
+        self.iv = SymmetricCryptor.randomDataOfLength(length)
     }
 }
--- CommonCryptoInSwift/ViewController.swift
+++ CommonCryptoInSwift/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // outlets && buttons
+    @IBOutlet weak var clearTextView: UITextView!
+    @IBOutlet weak var cypheredTextView: UITextView!
+    @IBOutlet weak var symmetricKeyLabel: UITextField!
+    
+    @IBOutlet weak var desButton: UIButton!
+    @IBOutlet weak var tripledesButton: UIButton!
+    @IBOutlet weak var rc4Button: UIButton!
+    @IBOutlet weak var rc2Button: UIButton!
+    @IBOutlet weak var aesButton: UIButton!
+    @IBOutlet weak var pkcs7Button: UIButton!
+    @IBOutlet weak var ebcButton: UIButton!
+    
+    // data
+    var selectedAlgorithm: SymmetricCryptorAlgorithm = .DES
+    var pkcs7Padding = false
+    var useEcbMode = false
+    var clearText: String = "Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."
+    var cypherText: NSData?
+    var iv: NSData?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        clearTextView.text = clearText
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +45,142 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Button actions
+    @IBAction func generateKey(sender: UIButton!) {
+        let key = SymmetricCryptor.randomStringOfLength(selectedAlgorithm.requiredKeySize())
+        self.symmetricKeyLabel.text = key
+    }
+    
+    @IBAction func cypher(sender: AnyObject) {
+        // validity checks
+        if clearTextView.text?.characters.count < 1 {
+            showAlertWithMessage("Please enter a valid clear text.")
+            return
+        } else { clearText = clearTextView.text! }
+        // build cryptor
+        let options = getCypheringOptions()
+        let cypher = SymmetricCryptor(algorithm: selectedAlgorithm, options: options)
+        if !useEcbMode { // IV needed if not in ECB mode
+            setIV(cypher)
+        }
+        if symmetricKeyLabel.text?.characters.count < 1 { generateKey(nil) }
+        // perform cyphering
+        do {
+            cypherText = try cypher.crypt(string: clearText, key: symmetricKeyLabel.text!)
+            if let cypheredString = NSString(data: cypherText!, encoding: NSUTF8StringEncoding) {
+                cypheredTextView.text = cypheredString as String
+            } else {
+                cypheredTextView.text = "(Result not printable as string): \(cypherText)"
+            }
+            clearTextView.text = ""
+        } catch {
+            self.showAlertWithMessage("Unable to perform cyphering on given text. \(error). Try enabling PKCS7 padding.")
+        }
+    }
+    
+    @IBAction func decypher(sender: AnyObject) {
+        // validity checks
+        if cypherText == nil {
+            showAlertWithMessage("No cypher text to decypher")
+            return
+        } else { clearText = clearTextView.text! }
+        // build cryptor
+        let options = getCypheringOptions()
+        let cypher = SymmetricCryptor(algorithm: selectedAlgorithm, options: options)
+        if !useEcbMode { // IV needed if not in ECB mode
+            setIV(cypher)
+        }
+        if symmetricKeyLabel.text?.characters.count < 1 { generateKey(nil) }
+        // perform cyphering
+        do {
+            let clearData = try cypher.decrypt(cypherText!, key: symmetricKeyLabel.text!)
+            if let clearDataAsString = NSString(data: clearData, encoding: NSUTF8StringEncoding) {
+                clearText = clearDataAsString as String
+                clearTextView.text = clearText
+                cypherText = nil
+            } else { clearTextView.text = "(Unable to generate a valid response string, probably wrong key or parameters, showing as NSData instead): \(clearData)" }
+            cypheredTextView.text = ""
+        } catch {
+            self.showAlertWithMessage("Unable to perform decyphering on given text. \(error)")
+        }
 
+    }
+    
+    @IBAction func selectDESAlgorithm(sender: UIButton) {
+        selectedAlgorithm = .DES
+        selectAlgorithmButton(sender)
+    }
+    
+    @IBAction func select3DESAlgorithm(sender: UIButton) {
+        selectedAlgorithm = .TRIPLEDES
+        selectAlgorithmButton(sender)
+    }
+    
+    @IBAction func selectRC4Algorithm(sender: UIButton) {
+        selectedAlgorithm = .RC4_128
+        selectAlgorithmButton(sender)
+    }
+    
+    @IBAction func selectRC2Algorithm(sender: UIButton) {
+        selectedAlgorithm = .RC2_128
+        selectAlgorithmButton(sender)
+    }
+    
+    @IBAction func visuallySelectAESAlgorithm(sender: UIButton) {
+        selectedAlgorithm = .AES_128
+        selectAlgorithmButton(sender)
+    }
+    
+    @IBAction func pkcs7ButtonTouched(sender: UIButton) {
+        self.pkcs7Padding = !self.pkcs7Padding
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.pkcs7Button.alpha = self.pkcs7Padding ? 1.0 : 0.20
+        }
+    }
+    
+    @IBAction func ebcButtonTouched(sender: UIButton) {
+        self.useEcbMode = !self.useEcbMode
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.ebcButton.alpha = self.useEcbMode ? 1.0 : 0.20
+        }
+    }
+    
+    private func selectAlgorithmButton(button: UIButton) {
+        self.symmetricKeyLabel.text = ""
+        var allButtons: [UIButton] = [desButton, tripledesButton, rc4Button, rc2Button, aesButton]
+        if let index = allButtons.indexOf(button) {
+            allButtons.removeAtIndex(index)
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+                for buttonToFade in allButtons { buttonToFade.alpha = 0.20 }
+                button.alpha = 1.0
+            })
+        }
+    }
+    
+    func setIV(cypher: SymmetricCryptor) {
+        if iv == nil {
+            cypher.setRandomIV()
+            iv = cypher.iv
+        } else { cypher.iv = iv! }
+    }
+    
+    func getCypheringOptions() -> Int {
+        // build cyphering options
+        var options = 0
+        if self.pkcs7Padding { options += kCCOptionPKCS7Padding }
+        if self.useEcbMode { options += kCCOptionECBMode }
+        return options
+    }
+
+    // MARK: - UIViewController alerts options.
+    func showAlertWithMessage(msg: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: nil, message: msg, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: completion)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
